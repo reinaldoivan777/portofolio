419e9293f460a4233f5b2877ebec838541cbedf8
Ivan <reinaldoivan777@gmail.com>
2020-01-19 22:23:00 +0700

    Updates

---
+++ precache-manifest.a28cb5ed730ac4b1a5e08084a2f66890.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecef344fc91a607f0a1731886aed839a",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "e85df319159bbb5d500e",
+    "url": "/portofolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "97927641b7c7554368d2",
+    "url": "/portofolio/static/js/2.35b9ef75.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portofolio/static/js/2.35b9ef75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e85df319159bbb5d500e",
+    "url": "/portofolio/static/js/main.496c5679.chunk.js"
+  },
+  {
+    "revision": "3d6e013e3b05828b8bad",
+    "url": "/portofolio/static/js/runtime-main.289a3b32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portofolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
